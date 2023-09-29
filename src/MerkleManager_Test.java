6cb0573386961c61d304584f8f2a78592feeeed8
Maya Nnaji <121353891+mayacoding04@users.noreply.github.com>
2023-09-28 19:40:48 -0700

    Group Lab: Merkle Madness Console App
    Group lab in which we created a game where users enter a Merkle root consisting of four words and then participate in a multithreaded challenge.

---
+++ src/MerkleManager_Test.java
@@ -0,0 +1,7 @@
+//Bricen Schaefer, Maya Nnaji, Mohammad Sayaf Hakimi, Walid Elassaad
+public class MerkleManager_Test {
+    public static void main(String[] args) {
+        MerkleManager mmTest = new MerkleManager();
+        mmTest.manage();
+    }
+}
